6ad75273f008f9aacd5f67020ea5356250243e2a
Alexey Glukharev <lwengine@gmail.com>
2022-10-16 12:12:00 -0700

    Timer setup UI

--- app/src/main/java/com/appspell/sportintervaltimer/TimerSetupContent.kt
+++ app/src/main/java/com/appspell/sportintervaltimer/TimerSetupContent.kt
@@ -1,23 +1,29 @@
 package com.appspell.sportintervaltimer
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Scaffold
+import androidx.wear.compose.material.ScalingLazyColumn
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
 import androidx.wear.compose.material.Vignette
@@ -48,52 +54,81 @@ fun TimerSetupContent() {
             )
         }
     ) {
-        SetUpIntervalsContent()
+        SetUpIntervalsContent(
+            onStart = {},
+            onSetsAdd = {},
+            onSetsRemove = {},
+            onWorkAdd = {},
+            onWorkRemove = {},
+            onRestAdd = {},
+            onRestRemove = {}
+        )
     }
 }
 
 @Composable
-private fun SetUpIntervalsContent() {
-    Column(
+private fun SetUpIntervalsContent(
+    onStart: () -> Unit,
+    onSetsAdd: () -> Unit,
+    onSetsRemove: () -> Unit,
+    onWorkAdd: () -> Unit,
+    onWorkRemove: () -> Unit,
+    onRestAdd: () -> Unit,
+    onRestRemove: () -> Unit,
+) {
+    val listState = rememberScalingLazyListState()
+
+    ScalingLazyColumn(
+        state = listState,
+        contentPadding = PaddingValues(top = 16.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(top = 24.dp)
     ) {
-        TimePickerRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            rowName = stringResource(id = R.string.timer_setup_set),
-            defaultValue = "00:00",
-            onButtonPlus = {},
-            onButtonMinus = {}
-        )
-        TimePickerRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            rowName = stringResource(id = R.string.timer_setup_work),
-            defaultValue = "00:00",
-            onButtonPlus = {},
-            onButtonMinus = {}
-        )
-        TimePickerRow(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally),
-            rowName = stringResource(id = R.string.timer_setup_rest),
-            defaultValue = "00:00",
-            onButtonPlus = {},
-            onButtonMinus = {}
-        )
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-        ) {
-            Text(text = stringResource(R.string.timer_setup_start_button))
+        item {
+            TimerSetupStartButton(
+                onClick = onStart
+            )
         }
+        item {
+            TimePickerRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                rowName = stringResource(id = R.string.timer_setup_set),
+                defaultValue = "00:00",
+                onButtonAdd = onSetsAdd,
+                onButtonRemove = onSetsRemove
+            )
+        }
+        item {
+            TimePickerRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                rowName = stringResource(id = R.string.timer_setup_work),
+                defaultValue = "00:00",
+                onButtonAdd = onWorkAdd,
+                onButtonRemove = onWorkRemove
+            )
+        }
+        item {
+            TimePickerRow(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                rowName = stringResource(id = R.string.timer_setup_rest),
+                defaultValue = "00:00",
+                onButtonAdd = onRestAdd,
+                onButtonRemove = onRestRemove
+            )
+        }
+        item {
+            TimerSetupStartButton(
+                onClick = onStart
+            )
+        }
+    }
+
+    // Scroll to the first item
+    LaunchedEffect(Unit) {
+        listState.scrollToItem(0)
     }
 }
 
@@ -102,21 +137,23 @@ private fun TimePickerRow(
     modifier: Modifier,
     rowName: String,
     defaultValue: String,
-    onButtonPlus: () -> Unit,
-    onButtonMinus: () -> Unit,
+    onButtonAdd: () -> Unit,
+    onButtonRemove: () -> Unit,
 ) {
     Row(
         modifier = modifier
             .padding(start = 16.dp, end = 16.dp, top = 4.dp, bottom = 0.dp)
     ) {
         Button(
-            onClick = { onButtonPlus() },
+            onClick = { onButtonAdd() },
+            colors = ButtonDefaults.secondaryButtonColors(),
             modifier = Modifier
                 .align(CenterVertically)
                 .size(BUTTON_SIZE)
         ) {
-            Text(
-                text = stringResource(id = R.string.button_plus),
+            Icon(
+                painter = painterResource(id = R.drawable.ic_minus),
+                contentDescription = stringResource(id = R.string.button_minus)
             )
         }
 
@@ -129,27 +166,42 @@ private fun TimePickerRow(
             Text(
                 text = rowName,
                 textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.caption3,
                 maxLines = 1,
                 modifier = Modifier.fillMaxWidth(),
             )
             Text(
                 text = defaultValue,
                 textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body1,
                 maxLines = 1,
                 modifier = Modifier.fillMaxWidth(),
             )
         }
 
         Button(
-            onClick = { onButtonMinus() },
+            onClick = { onButtonRemove() },
+            colors = ButtonDefaults.secondaryButtonColors(),
             modifier = Modifier
                 .align(CenterVertically)
                 .padding(top = 8.dp)
                 .size(BUTTON_SIZE)
         ) {
-            Text(
-                text = stringResource(id = R.string.button_minus),
+            Icon(
+                painter = painterResource(id = R.drawable.ic_plus),
+                contentDescription = stringResource(id = R.string.button_plus)
             )
         }
+    }
+}
+
+@Composable
+private fun TimerSetupStartButton(
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = { onClick() },
+    ) {
+        Text(text = stringResource(R.string.timer_setup_start_button))
     }
 }
