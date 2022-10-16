d9991696b7ae56c6a164045fe2d73d4e9d3bc3ca
Alexey Glukharev <lwengine@gmail.com>
2022-10-16 14:14:50 -0700

    Add Hilt

---
+++ app/src/main/java/com/appspell/sportintervaltimer/IntervalTimerApplication.kt
@@ -0,0 +1,9 @@
+package com.appspell.sportintervaltimer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IntervalTimerApplication : Application() {
+
+}
--- app/src/main/java/com/appspell/sportintervaltimer/MainActivity.kt
+++ app/src/main/java/com/appspell/sportintervaltimer/MainActivity.kt
@@ -3,7 +3,9 @@ package com.appspell.sportintervaltimer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
