aae239ca71799eed50f6599ef945b27315b46ae3
Alexey Glukharev <lwengine@gmail.com>
2022-11-05 12:16:05 -0700

    Add constraint layout for timer screen. Support rectangular screens

--- app/src/main/java/com/appspell/sportintervaltimer/timer/TimerContent.kt
+++ app/src/main/java/com/appspell/sportintervaltimer/timer/TimerContent.kt
@@ -1,23 +1,28 @@
 package com.appspell.sportintervaltimer.timer
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
@@ -50,11 +55,11 @@ fun TimerContent(
 
     LaunchedEffect(Unit) {
         viewModel.navigation.onEach { newNavigationEvent ->
-                navController.popBackStack()
-                navController.navigate(
-                    newNavigationEvent.route, NavOptions.Builder().setLaunchSingleTop(true).build()
-                )
-            }.launchIn(this)
+            navController.popBackStack()
+            navController.navigate(
+                newNavigationEvent.route, NavOptions.Builder().setLaunchSingleTop(true).build()
+            )
+        }.launchIn(this)
     }
 
     when (state.type) {
@@ -83,12 +88,26 @@ fun TimerContent(
 private fun TimerScreenContent(
     state: TimerUiState, onPause: () -> Unit, onResume: () -> Unit, onSkip: () -> Unit
 ) {
+    var isSquare by remember { mutableStateOf(true) }
+
     Scaffold(
-        modifier = Modifier.background(MaterialTheme.colors.background),
+        modifier = Modifier
+            .background(MaterialTheme.colors.background)
+            .onSizeChanged {
+                isSquare = it.width == it.height
+            },
         timeText = {
-            TimeText(
-                timeTextStyle = MaterialTheme.typography.caption2, modifier = Modifier.padding(8.dp)
-            )
+            if (isSquare) {
+                TimeText(
+                    timeTextStyle = MaterialTheme.typography.caption2,
+                    modifier = Modifier.padding(8.dp)
+                )
+            } else {
+                TimeText(
+                    timeTextStyle = MaterialTheme.typography.caption3,
+                    modifier = Modifier
+                )
+            }
         },
     ) {
         TimerCountDown(
@@ -117,15 +136,69 @@ private fun TimerCountDown(
     onResume: () -> Unit,
     onSkip: () -> Unit,
 ) {
-    // TODO replace with Constraint Layout, current implementation might have issues with different watch sizes
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize(),
     ) {
+        val (circularProgress, sets, activity, timer, actionButtons) = createRefs()
+        val verticalChain = createVerticalChain(
+            sets,
+            activity,
+            timer,
+            actionButtons,
+            chainStyle = ChainStyle.Packed
+        )
+
         CircularProgressIndicator(
             progress = progress,
             strokeWidth = 8.dp,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .constrainAs(circularProgress) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .aspectRatio(1f)
+        )
+
+        // sets left
+        Text(
+            text = setsText,
+            fontWeight = FontWeight.Light,
+            style = MaterialTheme.typography.caption1,
+            color = MaterialTheme.colors.secondary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .wrapContentSize()
+                .constrainAs(sets) {
+                    top.linkTo(circularProgress.top, 16.dp)
+                    bottom.linkTo(activity.top)
+                    start.linkTo(circularProgress.start)
+                    end.linkTo(circularProgress.end)
+                }
+        )
+
+        // type of activity
+        Text(
+            text = when (type) {
+                PREPARE -> stringResource(id = R.string.type_prepare)
+                WORK -> stringResource(id = R.string.type_work)
+                REST -> stringResource(id = R.string.type_rest)
+                UNDEFINED -> ""
+            },
+            fontWeight = FontWeight.Normal,
+            style = MaterialTheme.typography.caption2,
+            color = MaterialTheme.colors.secondaryVariant,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .wrapContentSize()
+                .constrainAs(activity) {
+                    top.linkTo(circularProgress.top)
+                    bottom.linkTo(activity.top)
+                    start.linkTo(circularProgress.start)
+                    end.linkTo(circularProgress.end)
+                }
         )
 
         // Time
@@ -135,61 +208,31 @@ private fun TimerCountDown(
             style = MaterialTheme.typography.display1,
             color = MaterialTheme.colors.primary,
             textAlign = TextAlign.Center,
+            modifier = Modifier
+                .wrapContentSize()
+                .constrainAs(timer) {
+                    top.linkTo(activity.bottom, 8.dp)
+                    bottom.linkTo(actionButtons.top, 8.dp)
+                    start.linkTo(circularProgress.start, 8.dp)
+                    end.linkTo(circularProgress.end, 8.dp)
+                }
         )
 
-        Column(
+        // Action buttons (play & skip)
+        TimerActionButtons(
             modifier = Modifier
-                .align(Alignment.Center)
-                .padding(8.dp)
-        ) {
-            Box(
-                contentAlignment = Alignment.BottomCenter,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .weight(0.4f)
-            ) {
-                Column(
-                    modifier = Modifier
-                ) {
-                    // sets left
-                    Text(
-                        text = setsText,
-                        fontWeight = FontWeight.Light,
-                        style = MaterialTheme.typography.caption1,
-                        color = MaterialTheme.colors.secondary,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-                    // type of activity
-                    Text(
-                        text = when (type) {
-                            PREPARE -> stringResource(id = R.string.type_prepare)
-                            WORK -> stringResource(id = R.string.type_work)
-                            REST -> stringResource(id = R.string.type_rest)
-                            UNDEFINED -> ""
-                        },
-                        fontWeight = FontWeight.Normal,
-                        style = MaterialTheme.typography.caption2,
-                        color = MaterialTheme.colors.secondaryVariant,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    )
-                }
-            }
-            Box(modifier = Modifier.weight(0.3f)) {
-                // Spacer
-            }
-            // Action buttons (play & skip)
-            TimerActionButtons(
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(8.dp),
-                isPaused = isPaused,
-                onPause = onPause,
-                onResume = onResume,
-                onSkip = onSkip
-            )
-        }
+                .wrapContentSize()
+                .constrainAs(actionButtons) {
+                    top.linkTo(timer.bottom)
+                    bottom.linkTo(circularProgress.bottom)
+                    start.linkTo(circularProgress.start)
+                    end.linkTo(circularProgress.end)
+                },
+            isPaused = isPaused,
+            onPause = onPause,
+            onResume = onResume,
+            onSkip = onSkip
+        )
     }
 }
 
@@ -209,7 +252,8 @@ private fun TimerActionButtons(
                 onClick = onPause,
                 modifier = Modifier
                     .size(46.dp)
-                    .padding(4.dp),
+                    .padding(start = 4.dp, end = 4.dp)
+                    .aspectRatio(1f),
             ) {
                 Icon(
                     painter = painterResource(id = drawable.ic_pause),
@@ -221,7 +265,8 @@ private fun TimerActionButtons(
                 onClick = onResume,
                 modifier = Modifier
                     .size(46.dp)
-                    .padding(4.dp),
+                    .padding(start = 4.dp, end = 4.dp)
+                    .aspectRatio(1f),
             ) {
                 Icon(
                     painter = painterResource(id = drawable.ic_play),
@@ -234,7 +279,8 @@ private fun TimerActionButtons(
             onClick = onSkip,
             modifier = Modifier
                 .size(46.dp)
-                .padding(4.dp),
+                .padding(start = 4.dp, end = 4.dp)
+                .aspectRatio(1f),
             colors = ButtonDefaults.iconButtonColors(),
         ) {
             Icon(
